397244b2aefc59634fa66715e6f848d65a7e49cb
Martin Pettersson <martin.pettersson@wemind.se>
2013-11-30 18:35:27 +0100

    Kattis 2opt original.

--- src/EuclideanTSP.java
+++ src/EuclideanTSP.java
@@ -1,29 +1,37 @@
-/**
- * Created with IntelliJ IDEA.
- * User: martinpettersson
- * Date: 2013-11-28
- * Time: 15:11
- * To change this template use File | Settings | File Templates.
- */
 import java.util.ArrayList;
+
 public class EuclideanTSP {
-    Kattio io = new Kattio(System.in, System.out);
-    ArrayList<Pair<Double, Double>> inputNodes = new ArrayList<Pair<Double, Double>>();
+    private Kattio io;
+    private ArrayList<Pair<Double, Double>> inputNodes = new ArrayList<Pair<Double, Double>>();
 
     public static void main(String[] args) {
         new EuclideanTSP();
     }
 
     EuclideanTSP() {
+        io = new Kattio(System.in, System.out);
         handleInput();
+ 
+        /*
+        inputNodes.add(new Pair(95.0129, 61.5432));
+        inputNodes.add(new Pair(23.1139, 79.1937));
+        inputNodes.add(new Pair(60.6843, 92.1813));
+        inputNodes.add(new Pair(48.5982, 73.8207));
+        inputNodes.add(new Pair(89.1299, 17.6266));
+        inputNodes.add(new Pair(76.2097, 40.5706));
+        inputNodes.add(new Pair(45.6468, 93.5470));
+        inputNodes.add(new Pair(1.8504, 91.6904));
+        inputNodes.add(new Pair(82.1407, 41.0270));
+        inputNodes.add(new Pair(44.4703, 89.3650));
+        */
+
         //printInput();
-        int[] res = greedyTour();
-        int[] res2 = twoOpt();
+        //int[] res = greedyTour();
+        int[] res = twoOpt();
+
         printTour(res);
-        System.out.println("Total length: " + getTotalLength(res));
-        System.out.println("---------------");
-        printTour(res2);
-        System.out.println("Total length: " + getTotalLength(res2));
+        //System.out.println("Total length: " + getTotalLength(res));
+
         io.close();
     }
 
@@ -49,8 +57,9 @@ public class EuclideanTSP {
     }
 
     void printInput() {
-        for(Pair<Double, Double> node : inputNodes)
+        for(Pair<Double, Double> node : inputNodes) {
             System.out.println(node.getL() + " " + node.getR());
+        }
     }
 
     int[] greedyTour() {
@@ -75,38 +84,58 @@ public class EuclideanTSP {
     double dist(int a, int b) {
         return Math.sqrt(
                 Math.pow((inputNodes.get(a).getL()-inputNodes.get(b).getL()),2)+
-                Math.pow((inputNodes.get(a).getR()-inputNodes.get(b).getR()),2));
-    }
-
-    int[] twoOptSwap(int[] route, int i, int j) {
-        int N = inputNodes.size();
-        int[] newRoute = new int[N];
-        for (int ii = 0; ii < i; ii++)
-            newRoute[ii] = route[ii];
-        for (int ii = i; ii <= j; ii++)
-            newRoute[j-(ii-i)] = route[ii];
-        for (int ii = j+1; ii < N; ii++)
-            newRoute[ii] = route[ii];
-        return newRoute;
+                        Math.pow((inputNodes.get(a).getR()-inputNodes.get(b).getR()),2));
     }
 
     int[] twoOpt() {
-        int N = inputNodes.size();
-        int[] tour =  new int[N];
-        for (int i = 0; i < N; i++) tour[i] = i;
-        double bestDist = getTotalLength(tour);
-        for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++) {
-                int[] newRoute = twoOptSwap(tour, i,j);
-                double newDist =  getTotalLength(newRoute);
-                if (newDist < bestDist){
-                    bestDist = newDist;
-                    tour = newRoute;
+        /*
+        repeat until no improvement is made {
+            start_again:
+            best_distance = calculateTotalDistance(existing_route)
+            for (i = 0; i < number of nodes eligible to be swapped - 1; i++) {
+                for (k = i + 1; k < number of nodes eligible to be swapped; k++) {
+                    new_route = 2optSwap(existing_route, i, k)
+                    new_distance = calculateTotalDistance(new_route)
+                    if (new_distance < best_distance) {
+                        existing_route = new_route
+                        goto start_again
+                    }
                 }
             }
         }
+        */
+        int N = inputNodes.size();
+        int[] tour =  greedyTour();
+
+        double bestDist = getTotalLength(tour);
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                int oldi = tour[i];
+                int oldj = tour[j];
+
+                tour[i] = oldj;
+                tour[j] = oldi;
+                double newDist =  getTotalLength(tour);
+
+                if (newDist < bestDist){
+                    bestDist = newDist;
+                    //System.out.println("-");
+                } else { // Revert...
+                    tour[i] = oldi;
+                    tour[j] = oldj;
+
+
+                    return tour;
+                }
+            }
+        }
+
         return tour;
     }
+
+
+
+
 }
 
 class Pair<L,R> {
@@ -121,5 +150,3 @@ class Pair<L,R> {
     public void setL(L l){ this.l = l; }
     public void setR(R r){ this.r = r; }
 }
-
-
