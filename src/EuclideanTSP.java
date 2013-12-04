a85b6658fa8be23fd2306095e4f33ad28478b5c0
Martin Pettersson <martin.pettersson@wemind.se>
2013-12-04 05:23:40 +0100

    Cool optimizations

--- src/EuclideanTSP.java
+++ src/EuclideanTSP.java
@@ -5,102 +5,41 @@
  * Time: 15:11
  * To change this template use File | Settings | File Templates.
  */
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.io.PrintWriter;
 
 public class EuclideanTSP {
     Kattio io = new Kattio(System.in, System.out);
     ArrayList<Pair<Double, Double>> inputNodes = new ArrayList<Pair<Double, Double>>();
-    private static boolean visualize = false;
-    private static final String FILE_NAME = "tour.vis";
-    private static PrintWriter writer;
     private final static long           TIME_LIMIT = 1500000000;
-    private final static int            GENERATIONS = 3;
-
     static long startTime;
+    private static double[][] DIST_MATRIX;
+    private static int N;
 
-    public static void main(String[] args) throws FileNotFoundException {
-
-
+    public static void main(String[] args) {
         startTime = System.nanoTime();
-
-        writer = new PrintWriter(FILE_NAME);
-        if(args.length != 0) {
-            if(args[0].equals("visualize")) visualize = true;
-        }
         new EuclideanTSP();
-        writer.close();
     }
 
-    EuclideanTSP() throws FileNotFoundException {
-
-        /*
-        int[] a = new int[]{1, 2, 3, 4, 5, 6};
-        int[] bla = twoOptSwap(a, 2, 4);
-        for (int i = 0; i < bla.length; i++) {
-            System.out.println("" + bla[i]);
-        }
-        */
-
-
-
+    EuclideanTSP() {
         handleInput();
-        //generateVisData();
-
+        DIST_MATRIX = new double[N][N];
         int[] res = greedyTour();
-        System.err.println("----------"+getTotalLength(res)+"-----------");
+        //System.err.println("----------"+getTotalLength(res)+"-----------");
         res = twoOpt(res);
-        System.err.println("----------"+getTotalLength(res)+"-----------");
-
-
+        //System.err.println("----------"+getTotalLength(res)+"-----------");
         printTour(res);
-
-
-    }
-
-    void generateVisData() throws FileNotFoundException {
-        writer.print("points = [");
-        for(Pair<Double, Double> node : inputNodes) {
-            writer.print("(" + node.getL() + ", " + node.getR() + ")");
-            if(!node.equals(inputNodes.get(inputNodes.size()-1)))
-                writer.print(",");
-        }
-        writer.println("]");
-        writer.println("vis = Visualiser(points)");
-    }
-
-    void moreVisData(int[] tour) throws FileNotFoundException {
-        writer.print("Visualiser.update([");
-        for(int i = 0; i < tour.length; i++) {
-            writer.print(tour[i]);
-            if(i != tour.length -1)
-                writer.print(", ");
-        }
-        writer.println("])");
-        writer.println("sleep(secs)");
     }
 
     void handleInput() {
-        int numberOfNodes = io.getInt();
-        for(int i = 0; i < numberOfNodes; i++) {
+        N = io.getInt();
+        for(int i = 0; i < N; i++) {
             Pair<Double, Double> node = new Pair<Double, Double>(io.getDouble(), io.getDouble());
             inputNodes.add(i,node);
         }
-    }
-
-    void handleInputJapan() {
-        int numberOfNodes = io.getInt();
-        for(int i = 0; i < numberOfNodes; i++) {
-            int tmp = io.getInt();
-            Pair<Double, Double> node = new Pair<Double, Double>(io.getDouble(), io.getDouble());
-            inputNodes.add(i,node);
-        }
-        System.err.println("DONE READING");
     }
 
     void printTour(int[] tour) {
-        for(int i = 0; i < tour.length; i++)
+        for(int i = 0; i < N; i++)
             System.out.println(tour[i]);
     }
 
@@ -112,14 +51,14 @@ public class EuclideanTSP {
         return total;
     }
 
-    int[] greedyTour() throws FileNotFoundException {
-        int N = inputNodes.size();
+    int[] greedyTour(){
         int[] tour = new int[N];
         boolean[] used = new boolean[N];
         tour[0] = 0;
         used[0] = true;
+        int best;
         for(int i = 1; i < N; i++) {
-            int best = -1;
+            best = -1;
             for(int j = 0; j < N; j++) {
                 if(!used[j] && (best == -1 || dist(tour[i-1],j) < dist(tour[i-1],best))) {
                     best = j;
@@ -133,65 +72,44 @@ public class EuclideanTSP {
     }
 
     double dist(int a, int b) {
-        return Math.sqrt(
-                Math.pow((inputNodes.get(a).getL()-inputNodes.get(b).getL()),2)+
-                        Math.pow((inputNodes.get(a).getR()-inputNodes.get(b).getR()),2));
+        double result;
+        if(DIST_MATRIX[a][b] != 0) {
+            result = DIST_MATRIX[a][b];
+        } else {
+            result = Math.sqrt(
+                    Math.pow((inputNodes.get(a).getL()-inputNodes.get(b).getL()),2)+
+                            Math.pow((inputNodes.get(a).getR()-inputNodes.get(b).getR()),2));
+            DIST_MATRIX[a][b] = result;
+
+        }
+        return result;
     }
 
-    int[] twoOpt(int[] tour) throws  FileNotFoundException{
-        int N = inputNodes.size();
-        double bestDistance = getTotalLength(tour);
-
+    int[] twoOpt(int[] tour) {
         boolean found = false;
+        double oldDist, newDist;
+        int leftIndex, rightIndex, leftValue, rightValue;
         do {
             for(int i = 0; i < N; i++) {
                 for(int k = i+1; k < N-2; k++) {
                     if(System.nanoTime() - startTime > TIME_LIMIT) { return tour; }
-                    //int[] newTour = twoOptSwap(tour, i, k);
-
-                    double oldDist = dist(tour[i == 0 ? N-1 : i-1], tour[i]) + dist(tour[k], tour[k == N-1 ? 0 : k+1]);
-                    double newDist = dist(tour[i == 0 ? N-1 : i-1], tour[k]) + dist(tour[i], tour[k == N-1 ? 0 : k+1]);
-
-
+                    oldDist = dist(tour[i == 0 ? N-1 : i-1], tour[i]) + dist(tour[k], tour[k == N-1 ? 0 : k+1]);
+                    newDist = dist(tour[i == 0 ? N-1 : i-1], tour[k]) + dist(tour[i], tour[k == N-1 ? 0 : k+1]);
                     if (newDist < oldDist) {
-
                         for (int index = 0; index <= (k-i)/2; index++) {
-                            int leftIndex = i+index;
-                            int rightIndex = k-index;
-                            int leftValue = tour[leftIndex];
-                            int rightValue = tour[rightIndex];
+                            leftIndex = i+index;
+                            rightIndex = k-index;
+                            leftValue = tour[leftIndex];
+                            rightValue = tour[rightIndex];
                             tour[leftIndex] = rightValue;
                             tour[rightIndex] = leftValue;
                         }
-
                         found = true;
-                        break;
                     }
                 }
             }
         } while (found);
-
         return tour;
-        }
-
-    int[] twoOptSwap(int[] tour, int i, int k) {
-        int[] newTour = new int[tour.length];
-        for(int j = 0; j < tour.length; j++) {
-            newTour[j] = tour[j];
-        }
-
-
-        for (int index = 0; index <= k-i; index++) {
-            int leftIndex = i+index;
-            int rightIndex = k-index;
-            int leftValue = newTour[leftIndex];
-            int rightValue = newTour[rightIndex];
-
-            newTour[leftIndex] = rightValue;
-            newTour[rightIndex] = leftValue;
-        }
-
-        return newTour;
     }
 
     class Pair<L,R> {
@@ -206,6 +124,5 @@ public class EuclideanTSP {
         public void setL(L l){ this.l = l; }
         public void setR(R r){ this.r = r; }
     }
-
 }
 
