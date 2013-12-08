f1c824d4e01d7955cac2019dd9e7a8307a967f5e
Martin Pettersson <martin.pettersson@wemind.se>
2013-12-08 10:45:07 +0100

    2.5 opt and a few optimizations

--- src/EuclideanTSP.java
+++ src/EuclideanTSP.java
@@ -5,10 +5,11 @@
  * Time: 15:11
  * To change this template use File | Settings | File Templates.
  */
+
 import java.util.ArrayList;
 
 public class EuclideanTSP {
-    Kattio io = new Kattio(System.in, System.out);
+    private static Kattio io = new Kattio(System.in, System.out);
     ArrayList<Pair<Double, Double>> inputNodes = new ArrayList<Pair<Double, Double>>();
     private final static long           TIME_LIMIT = 1500000000;
     static long startTime;
@@ -18,15 +19,16 @@ public class EuclideanTSP {
     public static void main(String[] args) {
         startTime = System.nanoTime();
         new EuclideanTSP();
+        io.close();
     }
 
     EuclideanTSP() {
         handleInput();
         DIST_MATRIX = new double[N][N];
         int[] res = greedyTour();
-        //System.err.println("----------"+getTotalLength(res)+"-----------");
-        res = twoOpt(res);
-        //System.err.println("----------"+getTotalLength(res)+"-----------");
+        // System.err.println("----------"+getTotalLength(res)+"-----------");
+        res = twoPointFiveOpt(res);
+        //    System.err.println("----------"+getTotalLength(res)+"-----------");
         printTour(res);
     }
 
@@ -51,7 +53,7 @@ public class EuclideanTSP {
         return total;
     }
 
-    int[] greedyTour(){
+    int[] greedyTour() {
         int[] tour = new int[N];
         boolean[] used = new boolean[N];
         tour[0] = 0;
@@ -60,8 +62,9 @@ public class EuclideanTSP {
         for(int i = 1; i < N; i++) {
             best = -1;
             for(int j = 0; j < N; j++) {
-                if(!used[j] && (best == -1 || dist(tour[i-1],j) < dist(tour[i-1],best)))
+                if(!used[j] && (best == -1 || dist(tour[i-1],j) < dist(tour[i-1],best))) {
                     best = j;
+                }
             }
             tour[i] = best;
             used[best] = true;
@@ -71,27 +74,30 @@ public class EuclideanTSP {
 
     double dist(int a, int b) {
         double result;
-        if(DIST_MATRIX[a][b] != 0)
+        if(DIST_MATRIX[a][b] != 0) {
             result = DIST_MATRIX[a][b];
-        else {
+        } else {
             result = Math.sqrt(
                     Math.pow((inputNodes.get(a).getL()-inputNodes.get(b).getL()),2)+
                             Math.pow((inputNodes.get(a).getR()-inputNodes.get(b).getR()),2));
             DIST_MATRIX[a][b] = result;
+
         }
         return result;
     }
 
-    int[] twoOpt(int[] tour) {
+    int[] twoPointFiveOpt(int[] tour) {
+        int N = inputNodes.size();
         boolean found = false;
-        double oldDist, newDist;
         int leftIndex, rightIndex, leftValue, rightValue;
+
         do {
+            found = false;
             for(int i = 0; i < N; i++) {
                 for(int k = i+1; k < N-2; k++) {
                     if(System.nanoTime() - startTime > TIME_LIMIT) { return tour; }
-                    oldDist = dist(tour[i == 0 ? N-1 : i-1], tour[i]) + dist(tour[k], tour[k == N-1 ? 0 : k+1]);
-                    newDist = dist(tour[i == 0 ? N-1 : i-1], tour[k]) + dist(tour[i], tour[k == N-1 ? 0 : k+1]);
+                    double oldDist = dist(tour[i == 0 ? N-1 : i-1], tour[i]) + dist(tour[k], tour[k == N-1 ? 0 : k+1]);
+                    double newDist = dist(tour[i == 0 ? N-1 : i-1], tour[k]) + dist(tour[i], tour[k == N-1 ? 0 : k+1]);
                     if (newDist < oldDist) {
                         for (int index = 0; index <= (k-i)/2; index++) {
                             leftIndex = i+index;
@@ -100,7 +106,83 @@ public class EuclideanTSP {
                             rightValue = tour[rightIndex];
                             tour[leftIndex] = rightValue;
                             tour[rightIndex] = leftValue;
-                        } found = true;
+                            //moreVisData(tour);
+                        }
+                        found = true;
+                        // break;
+                    }
+                }
+            }
+
+            for(int i = 0; i < N; i++) {
+                for(int k = 0; k < N; k++) {
+                    if(System.nanoTime() - startTime > TIME_LIMIT) { return tour; }
+
+                    if(k >= i+1) {
+                        //  if(System.nanoTime() - startTime > TIME_LIMIT) { return tour; }
+                        double oldDist = dist(tour[i == 0 ? N-1 : i-1], tour[i]) + dist(tour[k], tour[k == N-1 ? 0 : k+1]);
+                        double newDist = dist(tour[i == 0 ? N-1 : i-1], tour[k]) + dist(tour[i], tour[k == N-1 ? 0 : k+1]);
+                        if (newDist < oldDist) {
+                            for (int index = 0; index <= (k-i)/2; index++) {
+                                if(System.nanoTime() - startTime > TIME_LIMIT) { return tour; }
+                                leftIndex = i+index;
+                                rightIndex = k-index;
+                                leftValue = tour[leftIndex];
+                                rightValue = tour[rightIndex];
+                                tour[leftIndex] = rightValue;
+                                tour[rightIndex] = leftValue;
+                            }
+                            found = true;
+                            //    break;
+                        }
+                    }
+
+                    int v = i;
+                    int vPrev =     (i == 0) ? N-1 : i-1;
+                    int vNext =     (i == N-1) ? 0 : i+1;
+                    int eLeft   = k;
+                    int eRight  = (k == N-1) ? 0 : k+1;
+                    if (tour[v] == tour[eLeft] || tour[v] == tour[eRight])
+                        continue;
+                    double oldDist = dist(tour[vPrev], tour[v]) + dist(tour[v], tour[vNext]) + dist(tour[eLeft], tour[eRight]);
+                    double newDist = dist(tour[vPrev], tour[vNext]) + dist(tour[eLeft], tour[v]) + dist(tour[v], tour[eRight]);
+                    if (newDist < oldDist) {
+                        int current = tour[v];
+                        if (v < eLeft) {
+                            if (eLeft == N-1) {
+                                for (int index = v; index < N-1; index++) {
+                                    if(System.nanoTime() - startTime > TIME_LIMIT) { return tour; }
+                                    tour[index] = tour[index + 1];
+                                }
+                                tour[eLeft] = current;
+                            } else {
+
+                                for (int index = v; index < eRight; index++) {
+                                    if(System.nanoTime() - startTime > TIME_LIMIT) { return tour; }
+                                    tour[index] = tour[index + 1];
+                                }
+                                tour[eLeft] = current;
+                            }
+                            found = true;
+                            // break;
+                        } else if (v > eRight) {
+                            if (eRight == 0) {
+                                for (int index = v; index > 0; index--) {
+                                    if(System.nanoTime() - startTime > TIME_LIMIT) { return tour; }
+                                    tour[index] = tour[index - 1];
+                                }
+                                tour[eRight] = current;
+                            } else {
+                                for (int index = v; index > eLeft; index--) {
+                                    if(System.nanoTime() - startTime > TIME_LIMIT) { return tour; }
+                                    tour[index] = tour[index - 1];
+                                }
+                                tour[eRight] = current;
+                            }
+                            found = true;
+                            //   break;
+                        }
+
                     }
                 }
             }
@@ -120,5 +202,6 @@ public class EuclideanTSP {
         public void setL(L l){ this.l = l; }
         public void setR(R r){ this.r = r; }
     }
+
 }
 
---
+++ src/EuclideanTSP2.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: martinpettersson
+ * Date: 2013-12-06
+ * Time: 00:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class EuclideanTSP2 {
+}
